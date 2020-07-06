66979600d9acffb1d2590f98b6c450ab23c1a959
Kirby <aldricsaligo21@gmail.com>
2020-07-06 16:13:54 -0700

    Created URL, json path, session, page, and user prompt
    Created a URL for the site. A json path. A user prompt for a team number. Then a session with a page variable to .get url with.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from requests_html import HTMLSession
 from lxml import html
 import json
+
+url = 'https://www.thebluealliance.com/"
+data_json_path = "/Users/Kitsune/Src/RoboticsWebScraping/site_data.json"
+
 def main():
-    match_scores = (25, 25, 25, 5, 25, 25, 0, 25, 25, 0)
-    average = sum(match_scores) / len(match_scores)
-    print (average)
+    team_num = input("What team number do you want to find the average endgame score of?")
+    url += team_num
+    session = HTMLSession()
+    page = session.get(url)
+    
+    
+    # match_scores = [25, 25, 25, 5, 25, 25, 0, 25, 25, 0]
+    # average = sum(match_scores) / len(match_scores)
+    # print (average)
     pass
 
 if __name__ == "__main__":
